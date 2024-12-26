bbb74252a6b64ef0c6c0b4540dd2fd71bfaa754d
Huriya Syed <167598780+huriyasyed3@users.noreply.github.com>
2024-12-26 19:47:45 +0500

    sanity-blog

--- sanity-blog/src/sanity/lib/client.ts
+++ sanity-blog/src/sanity/lib/client.ts
@@ -5,7 +5,7 @@ import { apiVersion, dataset, projectId } from '../env'
 const client = createClient({
   projectId:"8ss2f04s",
   dataset:"production",
-  apiVersion:"",
+  apiVersion:"v2021-3-25",
   useCdn: false, // Set to false if statically generating pages, using ISR or tag-based revalidation
 })
 
